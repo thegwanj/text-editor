65f316844485b2ef8a8999850985ac6c439deb9d
unknown <gwanjuchung@yahoo.com>
2022-11-17 21:12:44 -0800

    Fixed database not correctly putting and getting data

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -19,7 +19,7 @@ export const putDb = async (content) => {
   const jateDb = await openDB('jate', 1);
   const tx = jateDb.transaction('jate', 'readwrite');
   const store = tx.objectStore('jate');
-  const request = store.add({ text: content });
+  const request = store.put({ id: 1, value: content });
 
   const result = await request;
   console.log('Data saved to the database', result);
@@ -34,11 +34,12 @@ export const getDb = async () => {
   const jateDb = await openDB('jate', 1);
   const tx = jateDb.transaction('jate', 'readonly');
   const store = tx.objectStore('jate');
-  const request = store.getAll();
+  const request = store.get(1);
 
   const result = await request;
   console.log('result.value', result);
-  return result;
+  console.log(result.value);
+  return result.value;
 };
 
 //console.error('getDb not implemented');
