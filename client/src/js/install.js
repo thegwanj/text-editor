45ea4158bbdee4bbfe4ef050ba8bbe97c9ae8be9
unknown <gwanjuchung@yahoo.com>
2022-11-17 21:13:12 -0800

    Updated installation functions

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -7,15 +7,18 @@ window.addEventListener('beforeinstallprompt', (event) => {
     butInstall.style.visibility = 'visible';
     textHeader.textContent = 'Click the button to install!';  
 
-    butInstall.addEventListener('click', () => {
-        event.prompt();
-        butInstall.setAttribute('disabled', true);
-        butInstall.textContent = 'Installed!';
-      });    
+    // butInstall.addEventListener('click', () => {
+    //     event.prompt();
+    //     butInstall.setAttribute('disabled', true);
+    //     butInstall.textContent = 'Installed!';
+    //   });    
 });
 
 // TODO: Implement a click event handler on the `butInstall` element
-//butInstall.addEventListener('click', async () => {});
+butInstall.addEventListener('click', () => {
+    butInstall.setAttribute('disabled', true);
+    butInstall.textContent = 'Installed!';
+});
 
 // TODO: Add an handler for the `appinstalled` event
 window.addEventListener('appinstalled', (event) => {
