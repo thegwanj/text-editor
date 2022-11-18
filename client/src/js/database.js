2bca6e92534e3d935e81b5e941968d95ae83bbfe
unknown <gwanjuchung@yahoo.com>
2022-11-17 17:55:50 -0800

    Added logic to get and read content from database

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -29,7 +29,16 @@ export const putDb = async (content) => {
 
 // TODO: Add logic for a method that gets all the content from the database
 export const getDb = async () => {
+  console.log('Calling the getDb');
 
+  const jateDb = await openDB('jate', 1);
+  const tx = jateDb.transaction('jate', 'readonly');
+  const store = tx.objectStore('jate');
+  const request = store.getAll();
+
+  const result = await request;
+  console.log('result.value', result);
+  return result;
 };
 
 //console.error('getDb not implemented');
